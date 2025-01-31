27cd18e81d300e55f06624e4219bb87653da705e
AditiPatil2814 <168960269+AditiPatil2814@users.noreply.github.com>
2025-02-01 00:10:31 +0530

    Update Contact.jsx

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,42 +1,53 @@
-
 import { Box, styled, Typography, Link } from '@mui/material';
 import { GitHub, Instagram, Email } from '@mui/icons-material';
 
 const Banner = styled(Box)`
-    background-image: url(http://mrtaba.ir/image/bg2.jpg);
+    background-image: url('http://mrtaba.ir/image/bg2.jpg');
     width: 100%;
     height: 50vh;
-    background-position: left 0px top -100px;
+    background-position: center;
     background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    font-size: 32px;
+    font-weight: bold;
+    text-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
 `;
 
 const Wrapper = styled(Box)`
-    padding: 20px;
-    & > h3, & > h5 {
-        margin-top: 50px;
-    }
+    padding: 40px 20px;
+    max-width: 800px;
+    margin: auto;
+    text-align: center;
 `;
 
 const Text = styled(Typography)`
-    color: #878787;
+    color: #4a4a4a;
+    font-size: 18px;
+    margin-top: 20px;
 `;
 
+const IconLink = styled(Link)`
+    margin: 0 10px;
+    color: #333;
+    &:hover {
+        color: #0072b1;
+    }
+`;
 
 const Contact = () => {
     return (
         <Box>
-            <Banner />
+            <Banner>Get in Touch</Banner>
             <Wrapper>
-                <Typography variant="h3">Getting in touch is easy!</Typography>    
-                <Text variant="h5">
-                    Reach out to me on
-                    <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
-                        <Instagram/>
-                    </Link>
-                    or send me an Email 
-                    <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
-                        <Email />
-                    </Link>.
+                <Typography variant="h3" gutterBottom>Getting in Touch is Easy!</Typography>    
+                <Text variant="body1">
+                    Send me an email  
+                    <IconLink href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank">
+                        <Email sx={{ fontSize: 30 }} />
+                    </IconLink>
                 </Text>
             </Wrapper>
         </Box>
